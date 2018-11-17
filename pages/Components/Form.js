55386af5c7aad9b0abfa9927d331c111f291de38
Rhys Brame <rhysbrame@rhyss-mbp.home>
2018-11-17 00:30:32 +0000

    added first and second name input

--- pages/Components/Form.js
+++ pages/Components/Form.js
@@ -1,9 +1,62 @@
 
-const Form = () => (
-    <div>
-        <p>Form Page</p>
-         <input></input>
-    </div>
-)
+
+class Form extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            firstName: '',
+            secondName: '',
+            value: ''
+        };
+        
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    
+    handleChange(event) {
+        const name = event.target.name
+        const value = event.target.value
+        this.setState({[name]: value});
+    }
+    
+    handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.firstName + this.state.secondName);
+        event.preventDefault();
+    }
+    
+    render() {
+        return (
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            First Name:
+            <input 
+                type="text" 
+                value={this.state.firstName} 
+                onChange={this.handleChange} 
+                className="form-control" 
+                name="firstName"
+                placeholder="First Name"
+            />
+          </label>
+          <label>
+            Second Name:
+            <input 
+                type="text" 
+                value={this.state.secondName} 
+                onChange={this.handleChange} 
+                className="form-control" 
+                name="secondName"
+                placeholder="Second Name"
+            />          
+        </label>
+          <label>
+          Value:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+          <input type="submit" value="Submit" />
+        </form>
+      );
+    }
+}
 
 export default Form
