52a56116199837c8356aa183291e4e346e2aeb60
Rhys Brame <rhysbrame@rhyss-mbp.home>
2018-11-18 20:11:03 +0000

    some styling additions

---
+++ pages/Components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="navbar navbar-dark bg-primary">
+      <span className="navbar-brand">Simple React.js form with validation</span>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
+import { Fragment } from "react";
+import Head from "next/head";
+import Header from "./Components/Header";
+import Jumbo from "./Components/Jumbo";
 import Form from "./Components/Form";
 
-const Index = () => (
-  <div>
-    <p>Simple React form with validation using Next.js</p>
+export default () => (
+  <Fragment>
+    <Head>
+      <title>Simple React form with validation using Next.js</title>
+    </Head>
+    <Header />
+    <Jumbo />
     <Form />
-  </div>
+  </Fragment>
 );
-
-export default Index;
