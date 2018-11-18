86d6e7b265e6eddc781799532908b5d2c5f91502
Rhys Brame <rhysbrame@rhyss-mbp.home>
2018-11-18 20:12:59 +0000

    form validation and increased fields

--- pages/Components/Form.js
+++ pages/Components/Form.js
@@ -1,5 +1,6 @@
-import React, { Component } from "react";
-import TextInput from "./TextInput";
+import React, { Component, Fragment } from "react";
+import FormInput from "./FormInput";
+import { FormErrors } from "./FormErrors";
 
 class Form extends Component {
   constructor(props) {
@@ -7,46 +8,183 @@ class Form extends Component {
     this.handleFirstNameChange = this.handleFirstNameChange.bind(this);
     this.handleSecondNameChange = this.handleSecondNameChange.bind(this);
     this.handleTownChange = this.handleTownChange.bind(this);
+    this.handlePhoneNumberChange = this.handlePhoneNumberChange.bind(this);
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
     this.state = {
       firstName: "",
       secondName: "",
-      town: ""
+      town: "",
+      phoneNumber: "",
+      email: "",
+      formErrors: {
+        firstName: "",
+        secondName: "",
+        town: "",
+        phoneNumber: "",
+        email: ""
+      },
+      firstNameValid: false,
+      secondNameValid: false,
+      townValid: false,
+      phoneNumberValid: false,
+      emailValid: false,
+      formValid: false
     };
   }
 
+  validateField(fieldName, value) {
+    let fieldValidationErrors = this.state.formErrors;
+    let firstNameValid = this.state.firstNameValid;
+    let secondNameValid = this.state.secondNameValid;
+    let townValid = this.state.townValid;
+    let phoneNumberValid = this.state.phoneNumberValid;
+    let emailValid = this.state.emailValid;
+
+    switch (fieldName) {
+      case "firstName":
+        firstNameValid = value.length >= 3;
+        fieldValidationErrors.firstName = firstNameValid ? "" : " is too short";
+        break;
+      case "secondName":
+        secondNameValid = value.length >= 3;
+        fieldValidationErrors.secondName = secondNameValid
+          ? ""
+          : " is too short";
+        break;
+      case "town":
+        townValid = value.length >= 3;
+        fieldValidationErrors.town = townValid ? "" : " is too short";
+        break;
+      case "email":
+        emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+        fieldValidationErrors.email = emailValid ? "" : " is invalid";
+        break;
+      case "phoneNumber":
+        phoneNumberValid = value.match(/^\d{11}$|^$/);
+        fieldValidationErrors.phoneNumber = phoneNumberValid
+          ? ""
+          : " not a valid phone number with 11 digits";
+        break;
+      default:
+        break;
+    }
+    this.setState(
+      {
+        formErrors: fieldValidationErrors,
+        emailValid: emailValid,
+        firstNameValid: firstNameValid,
+        secondNameValid: secondNameValid,
+        townValid: townValid,
+        phoneNumberValid: phoneNumberValid
+      },
+      this.validateForm
+    );
+  }
+
+  validateForm() {
+    this.setState({
+      formValid:
+        this.state.emailValid &&
+        this.state.firstNameValid &&
+        this.state.secondNameValid &&
+        this.state.townValid &&
+        this.state.phoneNumberValid
+    });
+  }
+
   handleFirstNameChange(value) {
-    console.log("***f***", this.state.firstName);
-    this.setState({ firstName: value });
+    this.setState({ firstName: value }, () => {
+      this.validateField("firstName", value);
+    });
   }
 
   handleSecondNameChange(value) {
-    console.log("***s***", this.state.secondName);
-    this.setState({ secondName: value });
+    this.setState({ secondName: value }, () => {
+      this.validateField("secondName", value);
+    });
   }
 
   handleTownChange(value) {
-    console.log("***t***", this.state.town);
-    this.setState({ town: value });
+    this.setState({ town: value }, () => {
+      this.validateField("town", value);
+    });
+  }
+
+  handlePhoneNumberChange(value) {
+    this.setState({ phoneNumber: value }, () => {
+      this.validateField("phoneNumber", value);
+    });
+  }
+
+  handleEmailChange(value) {
+    this.setState({ email: value.toLowerCase() }, () => {
+      this.validateField("email", value);
+    });
+  }
+
+  handleSubmit(event) {
+    const dataJSON = {
+      "First Name:": this.state.firstName,
+      "Second Name:": this.state.secondName,
+      "Town:": this.state.town,
+      "Phone Number:": this.state.phoneNumber,
+      "Email:": this.state.email
+    };
+    //keep this console.log here to send final data object
+    console.log("Form Data:", dataJSON);
+    event.preventDefault();
   }
 
   render() {
     const firstName = this.state.firstName;
     const secondName = this.state.secondName;
     const town = this.state.town;
+    const phoneNumber = this.state.phoneNumber;
+    const email = this.state.email;
 
     return (
-      <form>
-        <TextInput
-          value={firstName}
-          onInputChange={this.handleFirstNameChange}
-        />
-        <TextInput
-          value={secondName}
-          onInputChange={this.handleSecondNameChange}
-        />
-        <TextInput value={town} onInputChange={this.handleTownChange} />
-        <input type="submit" value="Submit" />
-      </form>
+      <Fragment>
+        <div style={{ ["textAlign"]: "center" }}>
+          <div>
+            <FormErrors formErrors={this.state.formErrors} />
+          </div>
+          <form onSubmit={this.handleSubmit}>
+            <FormInput
+              value={firstName}
+              onInputChange={this.handleFirstNameChange}
+              label="First Name:"
+            />
+            <FormInput
+              value={secondName}
+              onInputChange={this.handleSecondNameChange}
+              label="Second Name:"
+            />
+            <FormInput
+              value={town}
+              onInputChange={this.handleTownChange}
+              label="Town:"
+            />
+            <FormInput
+              value={phoneNumber}
+              onInputChange={this.handlePhoneNumberChange}
+              label="Phone Number:"
+            />
+            <FormInput
+              value={email}
+              onInputChange={this.handleEmailChange}
+              label="Email:"
+            />
+            <button
+              type="submit"
+              className="btn btn-primary"
+              disabled={!this.state.formValid}
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+      </Fragment>
     );
   }
 }
--- pages/tests/Form.spec.js
+++ pages/tests/Form.spec.js
@@ -1,11 +1,76 @@
 import * as React from "react";
-import { mount } from "enzyme";
+import { shallow } from "enzyme";
+import Form from "../Components/Form";
+import sinon from "sinon";
+import FormInput from "../Components/FormInput";
 
-describe("Form spec", () => {
-  it("should pass", () => {
-    expect(true).toBe(true);
+describe("Form spec", function() {
+  const getWrapper = props => shallow(<Form {...props} />);
+
+  it("should render form", () => {
+    const wrapper = getWrapper();
+    const form = wrapper.find("form");
+    expect(form.exists()).toBe(true);
   });
-  it("should render an input", () => {
-    expect(true).toBe(true);
+  it("should render FormErrors", function() {
+    const wrapper = getWrapper({
+      formErrors: {
+        foo: "bar"
+      }
+    });
+    const errors = wrapper.find("FormErrors");
+    expect(errors.exists()).toBe(true);
+  });
+  it("should execute correct method on submit", () => {
+    const handleSubmit = sinon.spy();
+    const wrapper = getWrapper({ handleSubmit });
+    const form = wrapper.find("form");
+    const e = { preventDefault: sinon.spy() };
+    form.simulate("submit", e);
+    expect(wrapper.find("form").exists()).toBe(true);
+    expect(e.preventDefault.called).toBe(true);
+    // expect(handleSubmit.called).toBe(true);
+  });
+  it("should have correct initial state", () => {
+    const wrapper = getWrapper();
+    const initialState = {
+      email: "",
+      emailValid: false,
+      firstName: "",
+      firstNameValid: false,
+      formErrors: {
+        email: "",
+        firstName: "",
+        phoneNumber: "",
+        secondName: "",
+        town: ""
+      },
+      formValid: false,
+      phoneNumber: "",
+      phoneNumberValid: false,
+      secondName: "",
+      secondNameValid: false,
+      town: "",
+      townValid: false
+    };
+    expect(wrapper.state()).toEqual(initialState);
+  });
+  describe("should pass the correct props", () => {
+    it("to the first name form input", () => {
+      const handleFirstNameChange = jest.fn();
+      const wrapper = getWrapper({
+        onInputChange: handleFirstNameChange()
+      });
+      const event = {
+        preventDefault() {},
+        target: { value: "the-value" }
+      };
+      const formInput = wrapper.find(FormInput).at(0);
+      expect(formInput.exists()).toBe(true);
+      expect(formInput.prop("value")).toBe("");
+      expect(formInput.prop("label")).toBe("First Name:");
+      // formInput.simulate("change", event);
+      // expect(handleFirstNameChange.mock.calls[0][0]).toBe("the-value");
+    });
   });
 });
