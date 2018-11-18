569504533fffa2b0e743fa2d3b25d3711ffc447b
Rhys Brame <rhysbrame@rhyss-mbp.home>
2018-11-18 20:12:15 +0000

    error messages component

---
+++ pages/tests/FormErrors.spec.js
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { shallow } from "enzyme";
+import { FormErrors } from "../Components/FormErrors";
+
+describe("FormErrors spec", () => {
+  it("should render without throwing an error", () => {
+    const wrapper = shallow(<FormErrors formErrors />);
+    expect(wrapper.hasClass("FormErrors"));
+  });
+});
