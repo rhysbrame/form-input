e65b8ac701b54bd3efb3a65d33907ca13762906f
Rhys Brame <rhysbrame@Rhyss-MacBook-Pro.local>
2018-11-19 09:46:37 +0000

    test for form errors

--- Components/Form/Tests/FormErrors.spec.js
+++ Components/Form/Tests/FormErrors.spec.js
@@ -2,11 +2,16 @@ import * as React from "react";
 import { shallow } from "enzyme";
 import { FormErrors } from "../FormErrors";
 
-describe("FormErrors spec", () => {
-  it("should render a message for each error passed to it");
-  /////////////////////////////////////////////////////
-  it("should render without throwing an error", () => {
-    const wrapper = shallow(<FormErrors formErrors />);
-    expect(wrapper.hasClass("FormErrors"));
+describe.only("FormErrors spec", () => {
+  it("should render a message for each error passed to it", () => {
+    const formErrors = {
+      firstName: "foo",
+      secondName: null,
+      town: null,
+      phoneNumber: null,
+      email: null
+    };
+    const wrapper = shallow(<FormErrors formErrors={formErrors} />);
+    expect(wrapper.text()).toBe("foo");
   });
 });
